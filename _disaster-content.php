32d749fd42b2eccb6cfcb71505b7bec5904dc668
Derek Teay <derek.teay@gmail.com>
2019-02-04 17:11:03 -0500

    Add sample output

---
+++ _disaster-content.php
@@ -0,0 +1,271 @@
+<?php
+$content = array(
+	"Tech Talk" => array(
+		array(
+			"title" => "Call for Code: Understanding Storm Surge with Jamie Rhome, National Hurricane Center",
+			"date" => "This is a sample description",
+			"url" => "https://youtu.be/a_N2sqOLZ-8 https://developer.ibm.com/videos/understanding-storm-surge-with-jamie-rhome-national-hurricane-center/",
+		),
+		array(
+			"title" => "Call for Code: Tech Talk with the Information Technology Disaster Resource Center",
+			"date" => "This is a sample description",
+			"url" => "https://youtu.be/r-mgN7Pk8Ro",
+		),
+		array(
+			"title" => "Disaster planning for tsunamis and other rare but devastating events (Professor Harry Yeh)",
+			"date" => "This is a sample description",
+			"url" => "https://youtu.be/Wyz-ETTPmlU",
+		),
+		array(
+			"title" => "Call for Code: Challenges in earthquake response with Ken Dueker",
+			"date" => "This is a sample description",
+			"url" => "https://youtu.be/0Af2ozFVdn4 . https://developer.ibm.com/videos/tsunami-discussion-with-professor-harry-yeh-dev-advocate-raj-singh/",
+		),
+		array(
+			"title" => "Call for Code: Challenges faced in aftermath of hurricanes in Puerto Rico",
+			"date" => "This is a sample description",
+			"url" => "https://youtu.be/jIE0ELcDuZY",
+		)
+	),
+	"Background" => array(
+		array(
+			"title" => "National Hurricane Center ",
+			"date" => "This is a sample description",
+			"url" => "https://www.nhc.noaa.gov",
+		),
+		array(
+			"title" => "Ready.gov hurricane preparedness guide",
+			"date" => "This is a sample description",
+			"url" => "https://www.nhc.noaa.gov",
+		),
+		array(
+			"title" => "What is the difference between a hurricane and typhoon?",
+			"date" => "This is a sample description",
+			"url" => "https://www.redcross.org/get-help/how-to-prepare-for-emergencies/types-of-emergencies/hurricane/hurricane-vs-typhoon.html#Differences",
+		),
+		array(
+			"title" => "Understanding Hurricanes ",
+			"date" => "This is a sample description",
+			"url" => "https://www.nasa.gov/audience/forstudents/k-4/stories/nasa-knows/what-are-hurricanes-k4.html",
+		),
+		array(
+			"title" => "National Hurricane Center - Storm Surge Guide",
+			"date" => "This is a sample description",
+			"url" => "https://www.nhc.noaa.gov/surge/",
+		),
+		array(
+			"title" => "National Weather Service - Flood Guide",
+			"date" => "This is a sample description",
+			"url" => "https://www.weather.gov/pbz/floods",
+		),
+		array(
+			"title" => "Ready.gov Flooding Preparedness Guide",
+			"date" => "This is a sample description",
+			"url" => "https://www.ready.gov/floods",
+		),
+		array(
+			"title" => "U.S. Geological Survey",
+			"date" => "This is a sample description",
+			"url" => "https://earthquake.usgs.gov",
+		),
+		array(
+			"title" => "NOAA",
+			"date" => "This is a sample description",
+			"url" => "EMPTY",
+		),
+		array(
+			"title" => "FEMA - Protecting yourself in an earthquake ",
+			"date" => "This is a sample description",
+			"url" => "https://www.fema.gov/national-earthquake-hazards-reduction-program-best-practices",
+		),
+		array(
+			"title" => "European Center for Geodynamics and seismology",
+			"date" => "This is a sample description",
+			"url" => "http://www.ecgs.lu",
+		),
+		array(
+			"title" => "Ready.gov volcano preparedness ",
+			"date" => "This is a sample description",
+			"url" => "https://www.ready.gov/volcanoes",
+		),
+		array(
+			"title" => "USGS Volcano hazards program ",
+			"date" => "This is a sample description",
+			"url" => "https://volcanoes.usgs.gov/index.html",
+		),
+		array(
+			"title" => "It's not just Hawaii: the US has 169 volcanoes that could erupt ",
+			"date" => "This is a sample description",
+			"url" => "https://www.nytimes.com/2018/05/14/us/us-active-volcanoes-hawaii.html",
+		),
+		array(
+			"title" => "European Center for Geodynamics and seismology",
+			"date" => "This is a sample description",
+			"url" => "http://www.ecgs.lu",
+		),
+		array(
+			"title" => "Ready.gov winter weather preparedness",
+			"date" => "This is a sample description",
+			"url" => "https://www.ready.gov/winter-weather",
+		),
+		array(
+			"title" => "Ready.gov Tornado preparedness ",
+			"date" => "This is a sample description",
+			"url" => "https://www.ready.gov/tornadoes",
+		),
+		array(
+			"title" => "NOAA Tornando Climatology Data",
+			"date" => "This is a sample description",
+			"url" => "EMPTY",
+		),
+		array(
+			"title" => "Severe Weather Information Centerr",
+			"date" => "This is a sample description",
+			"url" => "https://severe.worldweather.wmo.int",
+		)
+	),
+	"Research" => array(
+		array(
+			"title" => "National Center for Disaster Preparedness ",
+			"date" => "This is a sample description",
+			"url" => "https://ncdp.columbia.edu/research/research-portal/",
+		),
+		array(
+			"title" => "International Hurricane Relief Center, Florida International University ",
+			"date" => "This is a sample description",
+			"url" => "http://www.ihrc.fiu.edu",
+		),
+		array(
+			"title" => "National Severe Storms Library",
+			"date" => "This is a sample description",
+			"url" => "EMPTY",
+		),
+		array(
+			"title" => "Stony Brook Storm Surge Research Center",
+			"date" => "This is a sample description",
+			"url" => "https://stormy.msrc.sunysb.edu",
+		),
+		array(
+			"title" => "UC Berkeley Seismology Lab",
+			"date" => "This is a sample description",
+			"url" => "http://seismo.berkeley.edu/ERA/",
+		),
+		array(
+			"title" => "NASA volcano research",
+			"date" => "This is a sample description",
+			"url" => "https://solidearth.jpl.nasa.gov/PAGES/volc04.html",
+		),
+		array(
+			"title" => "World Organization of Volcano Observatorries ",
+			"date" => "This is a sample description",
+			"url" => "http://www.wovo.org",
+		),
+		array(
+			"title" => "Smithsonian Institute Global Volcanism Program ",
+			"date" => "This is a sample description",
+			"url" => "https://volcano.si.edu",
+		),
+		array(
+			"title" => "National Climate Assessment - Severe Weather ",
+			"date" => "This is a sample description",
+			"url" => "https://nca2014.globalchange.gov/highlights/report-findings/extreme-weather",
+		),
+		array(
+			"title" => "NOAA Severe Storm Labortoray ",
+			"date" => "This is a sample description",
+			"url" => "EMPTY",
+		)
+	),
+	"Video" => array(
+		array(
+			"title" => "Call for Code: Understanding Storm Surge with Jamie Rhome, National Hurricane Center",
+			"date" => "This is a sample description",
+			"url" => "https://youtu.be/a_N2sqOLZ-8  https://developer.ibm.com/videos/understanding-storm-surge-with-jamie-rhome-national-hurricane-center/",
+		),
+		array(
+			"title" => "Call for Code: Understanding Wildfires with Geoffrey Blackshire, Palo Alto Fire Department",
+			"date" => "This is a sample description",
+			"url" => "https://youtu.be/e3KD7SYmlxI",
+		),
+		array(
+			"title" => "Call For Code: Fighting fires with drone support",
+			"date" => "This is a sample description",
+			"url" => "https://youtu.be/9VE4ZxqUay0",
+		),
+		array(
+			"title" => "Understanding natural disaster response with Palo Alto Police Department",
+			"date" => "This is a sample description",
+			"url" => "https://youtu.be/oojRzM55i08",
+		),
+		array(
+			"title" => "Call For Code: Angel Diaz (IBM) & Mary Glackin (The Weather Company)",
+			"date" => "This is a sample description",
+			"url" => "https://youtu.be/tWxkQyPo9RQ",
+		),
+		array(
+			"title" => "Call For Code: Laurent Sauveur (UN Human Rights)",
+			"date" => "This is a sample description",
+			"url" => "https://youtu.be/CXmnFFUntqs",
+		),
+		array(
+			"title" => "Kait Parker from the Weather Channel: after disaster strikes",
+			"date" => "This is a sample description",
+			"url" => "https://youtu.be/ctIyS3yqQWE",
+		),
+		array(
+			"title" => "Call For Code: Trevor Riggen, CEO American Red Cross on the value of Call For Code",
+			"date" => "This is a sample description",
+			"url" => "https://youtu.be/OJ8EDEaxs0A",
+		)
+	),
+	"Data" => array(
+		array(
+			"title" => "USGS Earthquake Hazard's Program ",
+			"date" => "This is a sample description",
+			"url" => "https://earthquake.usgs.gov/earthquakes/",
+		),
+		array(
+			"title" => "USGS Real-time Earthquake Data Feed",
+			"date" => "This is a sample description",
+			"url" => "https://earthquake.usgs.gov/earthquakes/feed/",
+		),
+		array(
+			"title" => "Pacific Tsunami Warning Center",
+			"date" => "This is a sample description",
+			"url" => "http://ptwc.weather.gov",
+		),
+		array(
+			"title" => "Smithsonian Volcano Data Feed ",
+			"date" => "This is a sample description",
+			"url" => "https://volcano.si.edu/database/webservices.cfm",
+		),
+		array(
+			"title" => "Global Volcanic Activity - weekly reports",
+			"date" => "This is a sample description",
+			"url" => "https://volcano.si.edu/reports_weekly.cfm",
+		)
+	),
+	"Article" => array(
+		array(
+			"title" => "The Big One",
+			"date" => "This is a sample description",
+			"url" => "https://www.newyorker.com/magazine/2015/07/20/the-really-big-one",
+		),
+		array(
+			"title" => "Devastating Photos of Hawaii's Volcano Eruption",
+			"date" => "This is a sample description",
+			"url" => "https://www.popularmechanics.com/science/environment/g20264868/hawaii-volcano-eruption-2018-photos/",
+		),
+		array(
+			"title" => "What would happen if Yellowstone erupted?",
+			"date" => "This is a sample description",
+			"url" => "https://www.vox.com/2014/9/5/6108169/yellowstone-supervolcano-eruption",
+		),
+		array(
+			"title" => "Will extreme weather become even deadlier?",
+			"date" => "This is a sample description",
+			"url" => "https://www.dw.com/en/will-extreme-weather-become-even-deadlier/a-44651459",
+		)
+	)
+);
+?>
---
+++ _technical-content.php
@@ -0,0 +1,364 @@
+<?php
+$content = array(
+	"Code Pattern" => array(
+		array(
+			"title" => "Build a better blockchain network",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/patterns/build-a-blockchain-network/",
+		),
+		array(
+			"title" => "Deploy an asset transfer app using blockchain",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/patterns/deploy-an-asset-transfer-app-using-blockchain/",
+		),
+		array(
+			"title" => "Automate business processes via blockchain vents",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/patterns/automate-business-processes-via-blockchain-events/",
+		),
+		array(
+			"title" => "Create financial applications using APIs on the mainframe",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/patterns/create-financial-applications-using-apis-on-mainframe/",
+		),
+		array(
+			"title" => "Track donations using blockchain",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/patterns/track-donations-blockchain/",
+		),
+		array(
+			"title" => "Create a mobile handwritten Hangul translation app ",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/patterns/create-mobile-handwritten-hangul-translation-app/",
+		),
+		array(
+			"title" => "Stream and analyze Tweets to create a social media dashboard",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/patterns/analyze-twitter-handles-and-hashtags-for-sentiment-and-content/",
+		),
+		array(
+			"title" => "Deploy serverless multilingual conference room",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/patterns/deploy-serverless-multilingual-conference-room/",
+		),
+		array(
+			"title" => "Create an app to perform inteliigent searches on date",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/patterns/create-an-app-to-perform-intelligent-searches-on-data/",
+		),
+		array(
+			"title" => "Classify ICD-10 data with Watson",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/patterns/classify-icd-10-data-with-watson/",
+		),
+		array(
+			"title" => "Develop a web-based mobile health app that uses machine learning",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/patterns/develop-web-based-mobile-health-app-uses-machine-learning/",
+		),
+		array(
+			"title" => "Create and deploy a scoring model to predict heartrate failure",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/patterns/create-and-deploy-a-scoring-model-to-predict-heartrate-failure/",
+		),
+		array(
+			"title" => "Create visualizations to understand food insecurity",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/patterns/create-visualizations-to-understand-food-insecurity/",
+		),
+		array(
+			"title" => "Analyze open medical datasets to gain insights",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/patterns/analyze-open-medical-data-sets-to-gain-insights/",
+		),
+		array(
+			"title" => "Predict Wildlife Intensity ",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/patterns/predict-wildfire-intensity/",
+		),
+		array(
+			"title" => "Analyze industrial equipment for defects",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/patterns/analyze-industrial-equipment-defects/",
+		),
+		array(
+			"title" => "Detect voltage anomalies in household IoT devices",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/patterns/detect-voltage-anomalies-in-household-iot-devices/",
+		),
+		array(
+			"title" => "Develop an IoT asset tracking app using Blockchain",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/patterns/develop-an-iot-asset-tracking-app-using-blockchain/",
+		),
+		array(
+			"title" => "Use MQTT to stream real-time data",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/patterns/use-mqtt-stream-real-time-data/",
+		),
+		array(
+			"title" => "Locate and count items with object detection",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/patterns/locate-and-count-items-with-object-detection/",
+		),
+		array(
+			"title" => "Deploy and use a multi-framework deep learning platform on Kubernetes",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/patterns/deploy-and-use-a-multi-framework-deep-learning-platform-on-kubernetes/",
+		)
+	),
+	"Tech Talk" => array(
+		array(
+			"title" => "Build a secure, resilient, traceable and transparent supply network",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/videos/build-secure-resilient-traceable-and-transparent-supply-networks/",
+		),
+		array(
+			"title" => "Replay: Create and execute blockchain smart contracts",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/code/techtalks/replay-create-and-execute-blockchain-smart-contracts/",
+		),
+		array(
+			"title" => "Deply an asset transfer app using blockchain",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/code/techtalks/deploy-asset-transfer-app-using-blockchain/",
+		),
+		array(
+			"title" => "Use AI and botts to improve real-time commuunications",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/videos/use-ai-and-bots-to-improve-real-time-communications/",
+		),
+		array(
+			"title" => "Tensor Flow Hangul Recognition",
+			"date" => "This is a sample description",
+			"url" => "https://www.youtube.com/watch?v=Ynusw4RcyRY",
+		),
+		array(
+			"title" => "Stream and analyze Tweets to a [social media] dashboard using Watson",
+			"date" => "This is a sample description",
+			"url" => "https://www.youtube.com/watch?v=aWKi4f6gytc",
+		),
+		array(
+			"title" => "Understand, analyze, and predict health and nutrition needs to improve services with data science",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/videos/understand-analyze-and-predict-health-and-nutrition-needs/",
+		),
+		array(
+			"title" => "Produce visualizations for US food insecurity using IBM Data Science Experience, PixieDust, and Watson Analytics",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/videos/produce-visualizations-for-us-food-insecurity/",
+		),
+		array(
+			"title" => "Improve logistics based on traffic and weather activity to reduce the number of people affected",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/videos/improve-logistics-based-on-traffic-and-weather-activity/",
+		),
+		array(
+			"title" => "Collect and analyze device sensor data to take corrective or preventative action automatically",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/videos/collect-and-analyze-device-sensor-data-to-take-corrective-action/",
+		),
+		array(
+			"title" => "Classify vehicle damage images",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/videos/classify-vehicle-damage-images/",
+		),
+		array(
+			"title" => "Jumpstart your IoT implementation ",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/code/techtalks/jump-start-iot-implementation/",
+		),
+		array(
+			"title" => "Using Swift to create a sample photo-sharing app",
+			"date" => "This is a sample description",
+			"url" => "https://www.youtube.com/watch?v=n7Q-v40zejM",
+		)
+	),
+	"Blog Post" => array(
+		array(
+			"title" => "Blockchain for business: Getting to "hello world" ",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/code/2018/02/09/blockchain-for-business-getting-to-hello-world/",
+		),
+		array(
+			"title" => "Startups and blockchain: Generating interest, demonstrating benefits",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/code/2018/01/26/startups-blockchain-generating-interest-demonstrating-benefits/",
+		),
+		array(
+			"title" => "What Call for Code 2018 means to me ",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/code/2018/07/06/call-code-means/",
+		),
+		array(
+			"title" => "Honing your foreign language learning with TensorFlow",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/code/2017/11/03/honing-foreign-language-learning-tensorflow/",
+		),
+		array(
+			"title" => "Build a mobile back-end connected to Watson Language Translator API",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/code/2018/01/02/build-a-mobile-back-end-connected-to-watson-language-translator-api/",
+		),
+		array(
+			"title" => "Tone Analyzer service on IBM Cloud",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/code/2018/02/21/tone-analyzer-service-ibm-cloud/",
+		),
+		array(
+			"title" => "Chatbots and conversational design",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/code/2018/02/21/chatbots-conversational-design/",
+		),
+		array(
+			"title" => "Chat-room translation using Watson, MQTT, OpenWhisk, and Twilio",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/code/2018/03/14/chat-room-translation-using-watson-mqtt-openwhisk-and-twilio/",
+		),
+		array(
+			"title" => "Giving drought refugees in East Africa a voice through mobile- and container-based back-end platforms",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/announcements/giving-drought-refugees-east-africa-voice-mobile-container-based-back-end-platforms/",
+		),
+		array(
+			"title" => "Visualizing food insecurity in DSX with Pixie Dust and Watson Analytics",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/code/2018/02/09/visualizing-food-insecurity-dsx-pixie-dust-watson-analytics/",
+		),
+		array(
+			"title" => "Build your Call for Code application with The Weather Company APIs",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/code/2018/06/18/build-your-call-for-code-application-with-the-weather-company-apis/",
+		),
+		array(
+			"title" => "Let's hear the weather today ",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/code/2018/01/22/lets-hear-weather-today/",
+		),
+		array(
+			"title" => "Advanced analytics applications in IoT",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/code/2017/11/17/advanced-analytics-applications-iot/",
+		),
+		array(
+			"title" => "How IoT could unleash the real power of the machine learning",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/code/2018/01/04/iot-unleash-real-power-machine-learning/",
+		),
+		array(
+			"title" => "Internet of Garbage (IoG): Serverless and IoT architecture for Smart Waste Management System",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/code/2018/02/09/internet-garbage-iog-serverless-iot-architecture-smart-waste-management-system/",
+		),
+		array(
+			"title" => "Use computer vision to detect and track moving objects in video",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/code/2018/05/11/using-computer-vision-to-detect-and-track-moving-objects-in-video/",
+		),
+		array(
+			"title" => "Object detection with AI vision is getting easier for developers",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/code/2018/02/13/object-detection-ai-vision-getting-easier-developers/",
+		),
+		array(
+			"title" => "Check Out Kitura and other Updates to BluePic!",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/code/2017/11/14/check-out-kitura-updates-with-bluepic/",
+		),
+		array(
+			"title" => "Industrial use case of Watson Visual Recognition",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/code/2017/11/14/industrial-use-case-watson-visual-recognition/",
+		),
+		array(
+			"title" => "IBM’s Watson Visual Recognition service to support Apple Core ML technology",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/code/2018/03/21/ibm-watson-visual-recognition-service-to-support-apple-core-ml/",
+		)
+	),
+	"Tutorial" => array(
+		array(
+			"title" => "Stand up a Hyperledger Fabric network on a single Ubuntu machine",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/tutorials/stand-up-hyperledger-fabric-network-using-one-server/",
+		),
+		array(
+			"title" => "Deploying a private Ethereum blockchain to IBM Cloud Kubernetes Service",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/tutorials/deploying-ethereum-blockchain-to-IBM-Container/",
+		),
+		array(
+			"title" => "Build an application with the Watson Android SDK in Android Studio",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/tutorials/use-watson-android-sdk-in-android-studio/",
+		),
+		array(
+			"title" => "Adventures in IoT -- TJBot, and mobile augmented reality",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/code/techtalks/adventures-iot-tjbot-mobile-augmented-reality/",
+		),
+		array(
+			"title" => "Mapbox webservices APIs",
+			"date" => "This is a sample description",
+			"url" => "https://docs.mapbox.com/api/#directions",
+		),
+		array(
+			"title" => "How to generate API key and token in IBM Watson IoT Platform",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/tutorials/iot-generate-apikey-apitoken/",
+		),
+		array(
+			"title" => "Build and deploy a PowerAI Vision model and use it in an iOS app",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/tutorials/build-and-deploy-a-powerai-vision-model-and-use-it-in-ios/",
+		),
+		array(
+			"title" => "Detect objects with PowerAI Vision",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/tutorials/powerai-vision-object-detection/",
+		),
+		array(
+			"title" => "Train a model with Watson Visual Recognition",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/tutorials/train-VisRec-model-free/",
+		)
+	),
+	"Data Sets" => array(
+		array(
+			"title" => "Healthcare.gov ",
+			"date" => "This is a sample description",
+			"url" => "https://healthdata.gov/search/type/dataset",
+		),
+		array(
+			"title" => "Global Health Observatory data repository",
+			"date" => "This is a sample description",
+			"url" => "http://apps.who.int/gho/data/node.home",
+		),
+		array(
+			"title" => "Weather",
+			"date" => "This is a sample description",
+			"url" => "https://developer.ibm.com/code/2018/06/18/build-your-call-for-code-application-with-the-weather-company-apis/",
+		),
+		array(
+			"title" => "Transportation",
+			"date" => "This is a sample description",
+			"url" => "https://www.openstreetmap.org/#map=5/38.007/-95.844",
+		),
+		array(
+			"title" => "Elevation",
+			"date" => "This is a sample description",
+			"url" => "https://data.usgs.gov/datacatalog/#fq=keywords%3A(%22elevation%22)&q=*%3A*",
+		),
+		array(
+			"title" => "Satellite Imagery ESA",
+			"date" => "This is a sample description",
+			"url" => "https://earth.esa.int/web/guest/data-access",
+		),
+		array(
+			"title" => "Satellite Imagery NASA",
+			"date" => "This is a sample description",
+			"url" => "https://earthdata.nasa.gov/earth-observation-data/imagery",
+		)
+	)
+);
+?>
